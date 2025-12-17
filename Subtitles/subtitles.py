fd04605566b72057ff419027ec7b9bdbe653216a
srshkn <snuqlstyas@gmail.com>
2025-12-17 15:56:18 +0500

    add subtitles

---
+++ Subtitles/subtitles.py
@@ -0,0 +1,12 @@
+import torch
+
+
+from transformers import WhisperForConditionalGeneration, WhisperProcessor, pipeline
+
+
+class Subtitles:
+    def extract_voice(video: any):
+        pass
+
+    def apply(voice: any):
+        pass
